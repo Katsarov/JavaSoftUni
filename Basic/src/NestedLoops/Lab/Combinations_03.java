bcf45ca043383872ff64c4afd3d38eba667c42e7
Kalin Katsarov <k.l.katsarov@gmail.com>
2022-07-27 17:38:04 +0300

    Any tasks solutions.

---
+++ Basic/src/NestedLoops/Lab/Combinations_03.java
@@ -0,0 +1,24 @@
+package NestedLoops.Lab;
+
+import java.util.Scanner;
+
+public class Combinations_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int counter = 0;
+
+        for (int x1 = 0; x1 <= n; x1++) {
+            for (int x2 = 0; x2 <= n; x2++) {
+                for (int x3 = 0; x3 <= n; x3++) {
+                    if(x1 + x2 + x3 == n){
+                        counter++;
+                    }
+                }
+            }
+        }
+        System.out.println(counter);
+    }
+}
