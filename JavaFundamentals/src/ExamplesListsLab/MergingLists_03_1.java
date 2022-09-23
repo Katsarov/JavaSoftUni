62b4ed93800b8a7beb7c16e3a6c680de00b4310c
Kalin Katsarov <k.l.katsarov@gmail.com>
2022-09-23 18:48:58 +0300

    Merging lists 03.1

---
+++ JavaFundamentals/src/ExamplesListsLab/MergingLists_03_1.java
@@ -0,0 +1,48 @@
+package ExamplesListsLab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class MergingLists_03_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> sortedA = nextLineOfInts(scanner);   // 10 6 3 2 1
+        List<Integer> sortedB = nextLineOfInts(scanner);   //  9 8 4 0
+
+        int indexA = 0;
+        int indexB = 0;
+
+        List<Integer> mergedList = new ArrayList<>();
+
+        while (indexA < sortedA.size() || indexB < sortedB.size()) {
+
+            if (indexA < sortedA.size()) {
+                mergedList.add(sortedA.get(indexA));
+                indexA++;
+            } else {
+                mergedList.add(sortedB.get(indexB));
+                indexB++;
+            }
+        }
+
+        for (Integer number : mergedList) {
+            System.out.print(number + " ");
+        }
+    }
+
+    private static List<Integer> nextLineOfInts(Scanner scanner) {
+        List<Integer> numbers = new ArrayList<>();
+
+        String[] input = scanner.nextLine().split(" ");
+//        String input = scanner.nextLine();
+//        String[] splittedInput = input.split(" ");
+
+        for (String s : input) {
+            numbers.add(Integer.parseInt(s));
+        }
+        return numbers;
+
+    }
+}
