6fe2470320942aed0dd94ab60e562717eca93623
Kalin Katsarov <k.l.katsarov@gmail.com>
2022-10-01 19:09:44 +0300

    Example 03

---
+++ JavaFundamentals/src/ExamplesListsExercises/Example_03.java
@@ -0,0 +1,14 @@
+package ExamplesListsExercises;
+
+import java.util.Scanner;
+
+public class Example_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = "Kalin";
+        name = name + " Katsarov";
+
+        System.out.println(name);
+    }
+}
