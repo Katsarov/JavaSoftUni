4547e3349088267c88a8d281c071071a5cbcd23d
Kalin Katsarov <k.l.katsarov@gmail.com>
2022-10-02 09:14:44 +0300

    House party 03

---
+++ JavaFundamentals/src/ExamplesListsExercises/HouseParty_03.java
@@ -0,0 +1,36 @@
+package ExamplesListsExercises;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class HouseParty_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        List<String> guests = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String line = scanner.nextLine();
+            String[] data = line.split("\\s+");
+            String name = data[0];
+
+            if (!line.contains("is not going!")) {
+                if (guests.contains(name)) {
+                    System.out.printf("%s is already in the list!\n", name);
+                } else {
+                    guests.add(name);
+                }
+            } else {
+                if (guests.contains(name)) {
+                    guests.remove(name);
+                } else {
+                    System.out.printf("%s is not in the list!\n", name);
+                }
+            }
+        }
+
+        guests.forEach(guest -> System.out.println(guest));
+    }
+}
