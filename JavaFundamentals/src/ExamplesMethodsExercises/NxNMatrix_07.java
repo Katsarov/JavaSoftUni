f967eb9b24c97f4342afcc7b56a33649fe55d4b1
Kalin Katsarov <k.l.katsarov@gmail.com>
2022-10-16 15:22:02 +0300

    N x N matrix 07

---
+++ JavaFundamentals/src/ExamplesMethodsExercises/NxNMatrix_07.java
@@ -0,0 +1,22 @@
+package ExamplesMethodsExercise;
+
+import java.util.Scanner;
+
+public class NxNMatrix_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        printNxNMatrix(n);
+    }
+
+    private static void printNxNMatrix(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
+    }
+}
